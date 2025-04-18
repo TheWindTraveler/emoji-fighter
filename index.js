e4032e6d13f88e064f862c2ad07a86983703f3e0
Begotten-Traveler <solomondev002@gmail.com>
2025-04-18 16:32:36 +0100

    chores: random fighters function

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ let fighters = [
     "🦁"
 ]
 
+let stageEl = document.getElementById("stage");
+let pickFightersEl = document.getElementById("pick-fighters");
+
+pickFightersEl.addEventListener("click", function(){
+    let randomFightersOne = Math.floor(Math.random() * fighters.length);
+    let randomFightersTwo = Math.floor(Math.random() * fighters.length);
+    
+
+    stageEl.textContent = fighters[randomFightersOne] + "VS" + fighters[randomFightersTwo];
+})
